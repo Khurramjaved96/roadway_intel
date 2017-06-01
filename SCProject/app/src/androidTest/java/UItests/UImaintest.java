8fc757c895dac53a92b443b18d0c5af94a6c5b64
Noor <14besenzia@seecs.edu.pk>
2017-06-01 16:03:25 +0500

    Updated test cases

--- SCProject/app/src/androidTest/java/UItests/UImaintest.java
+++ SCProject/app/src/androidTest/java/UItests/UImaintest.java
@@ -7,30 +7,18 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Camera;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import com.example.noor.scproject.MainActivity;
 import com.example.noor.scproject.R;
-import com.example.noor.scproject.StartActivity;
 import com.example.noor.scproject.settings.SettingsActivity;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -39,9 +27,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class UImaintest {
 
     @Rule
-    public IntentsTestRule<StartActivity> mActivityRule = new IntentsTestRule<>(
-            StartActivity.class);
-    private StartActivity mainActivity;
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
+            MainActivity.class);
+    private MainActivity mainActivity;
 
 
 
@@ -49,15 +37,23 @@ public class UImaintest {
     public void setActivity() {
         mainActivity = mActivityRule.getActivity();
     }
-
-
-
-    @Test
-    public void main_ActivityTest(){
-
-        onView(withId(R.id.start))
+   @Test
+    public void settings_Activity() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Settings"))
                 .perform(click());
-        intended(hasComponent(MainActivity.class.getName()));
+        //Check if intent with Activity 2 it's been launched
+        intended(hasComponent(SettingsActivity.class.getName()));
+    }
+
+
+
+  @Test
+    public void gallery_Activity() {
+        //openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withId(R.id.action_gallery))
+                .perform(click());
+
     }
 
 
--- SCProject/app/src/androidTest/java/UItests/UIsettingstest.java
+++ SCProject/app/src/androidTest/java/UItests/UIsettingstest.java
@@ -3,42 +3,20 @@ package UItests;
 /**
  * Created by noor on 6/1/17.
  */
-import org.hamcrest.Matcher;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Camera;
 import android.preference.Preference;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import com.example.noor.scproject.MainActivity;
-import com.example.noor.scproject.R;
-import com.example.noor.scproject.StartActivity;
 import com.example.noor.scproject.settings.SettingsActivity;
-
 import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
